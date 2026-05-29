dd545eb8743a019e0b79d76d122890587d65bd8e
Joaqui_feels <marquezzz0411@gmail.com>
2026-05-29 00:12:09 -0300

    Add comment explaining @EnableConfigurationProperties usage
    Added comment to explain the importance of @EnableConfigurationProperties annotation.

--- src/main/java/ar/edu/utn/ba/ddsi/countries/CountriesApplication.java
+++ src/main/java/ar/edu/utn/ba/ddsi/countries/CountriesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RestCountriesProperties.class)
+@EnableConfigurationProperties(RestCountriesProperties.class) //esto es IMPORTANTISIMO. Es NECESARIO para que springboot sepa que tiene que ir a instaciar esa clase (Donde se habilita la url api) y hacer toda la configuracion por nosotros.
 public class CountriesApplication {
 
 	public static void main(String[] args) {
