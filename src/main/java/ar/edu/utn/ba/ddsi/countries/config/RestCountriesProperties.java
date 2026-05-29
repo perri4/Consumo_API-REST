bf165328cb8fe229b32df02f3c761add9b37b0d4
Joaqui_feels <marquezzz0411@gmail.com>
2026-05-29 00:10:24 -0300

    Explicaciones de vinculacion
    Added comments explaining the purpose of annotations.

--- src/main/java/ar/edu/utn/ba/ddsi/countries/config/RestCountriesProperties.java
+++ src/main/java/ar/edu/utn/ba/ddsi/countries/config/RestCountriesProperties.java
@@ -3,8 +3,8 @@ package ar.edu.utn.ba.ddsi.countries.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "rest-countries")
-@Data
+@ConfigurationProperties(prefix = "rest-countries") //esto es propio de springBoot. Llama al archivo "application.yml" y se fija ahi, todo lo que hay debajo de un "rest-countries"
+@Data //esto basicamente habilita los getters y setter de la clase.    
 public class RestCountriesProperties {
 
     private String baseUrl;
